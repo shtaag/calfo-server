4a37ff1c9325dd3ca964b26cbfb29189a80577fc
Shigekazu Takei <takei.shg@gmail.com>
2012-07-09 01:35:54 +0900

    added json

--- calfo-server/Foundation.hs
+++ calfo-server/Foundation.hs
@@ -32,6 +32,7 @@ import Text.Jasmine (minifym)
 import Web.ClientSession (getKey)
 import Text.Hamlet (hamletFile)
 
+
 -- | The site argument for your application. This can be a good place to
 -- keep settings and values requiring initialization before your application
 -- starts running, such as database connections. Every handler will have
